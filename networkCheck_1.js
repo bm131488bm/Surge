9faa3292120582d44182de8372ff9c1ceac63be7
bm131488bm <72439161+bm131488bm@users.noreply.github.com>
2022-05-29 08:30:10 +0800

    Create networkCheck_1.js

---
+++ networkCheck_1.js
@@ -0,0 +1,13 @@
+
+#!name=Network Info Panel
+#!desc=網路詳情面板 @Nebulosa-Cat
+#!system=ios
+
+[Panel]
+NetInfoPanel=title="",content="",style=info,script-name=net-info-panel.js,update-interval=1
+
+[Script]
+net-info-panel.js=script-path=https://raw.githubusercontent.com/bm131488bm/Surge/main/networkCheck.js?token=GHSAT0AAAAAABT3MA2AS5R4QHKRZOX6PP6SYUSX2HA,type=generic
+
+[Host]
+ip-api.com = 208.95.112.1
