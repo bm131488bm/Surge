7f65221b9d696b4097e9841310916df53f25d2fd
bm131488bm <72439161+bm131488bm@users.noreply.github.com>
2022-05-29 09:37:43 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Surge
+# Whosis-Sayings
+
+新買的域名不知道放什麼的話可以參考看看ㄛ～
---
+++ public/main.js
@@ -0,0 +1,37 @@
+/* global Vue */
+(async () => {
+  const gravatar = {
+    hirakujira: { name: 'Hiraku', avatar: 'bf73e08d8bc1db95b62f02d50f8a03e9' },
+    Shawn_N: { name: '$踢低吸 八嘎 NONO 麻ㄙㄟ麻ㄙㄟ', avatar: '34624582cd585ba65e5b5368c84cb1a2' },
+    bill85101: {name: '森喵', avatar: '9e202866b38b7255f282beb005576731'}
+  }
+  const file = await fetch('public/saying.txt')
+  const text = await file.text()
+  const saying = text.split(';').slice(0, -1).map(x => x.trim())
+  new Vue({ // eslint-disable-line no-new
+    el: '#app',
+    data: {
+      saying: saying,
+      uname: '',
+      name: '',
+      avatar: 'https://www.gravatar.com/avatar/00000000000000000000000000000000',
+      message: ''
+    },
+    methods: {
+      updateSaying () {
+        const idx = Math.floor(Math.random() * this.saying.length)
+        const saying = this.saying[idx].split(',')
+        this.uname = saying[0]
+        this.name = gravatar[this.uname].name
+        this.avatar = `https://www.gravatar.com/avatar/${gravatar[this.uname].avatar}`
+        this.message = saying[1]
+      }
+    },
+    created () {
+      (function f () {
+        this.updateSaying()
+        return setTimeout(f.bind(this), 5000)
+      }).bind(this)()
+    }
+  })
+})()
